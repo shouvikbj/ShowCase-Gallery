bc9baf9b682af450269db855ab8a0a499c51a9a8
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-01-22 16:33:32 +0530

    adding PWA feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
 
   const loadContent = () => {
     return (
-      <div className="container">
+      <div className="">
         {loading ? (
           <div class="text-center">
             <br />
